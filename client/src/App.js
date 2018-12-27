59c79bcf95d3d72e376f7437d21689a482f6c3f8
Hal Peters <geohalbert@gmail.com>
2018-12-27 11:55:05 -0600

    test
    test

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import MissingParts from "./Containers/MissingPartsContainer";
 import ResolvedParts from "./Containers/ResolvedPartsContainer";
 import LabelParts from "./Containers/LabelPartsContainer";
 import CreateParts from "./Containers/CreatePartsContainer";
-import EditPart from "./Containers/EditPartContainer";
+// import EditPart from "./Containers/EditPartContainer";
 import EditParts from "./Containers/EditPartsContainer"
 import {Link} from "react-router-dom";
 import {BrowserRouter,Route,Switch} from "react-router-dom";
