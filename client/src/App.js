bf350ec172c3dd76e22fe3168aab94c4d6120e7d
Hal Peters <geohalbert@gmail.com>
2018-12-27 21:17:06 -0600

    testing repo
    testing repo

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import 'semantic-ui-css/semantic.min.css';
 import "./App.css";
 import Main from "./Components/Main";
 import Part from "./Containers/PartContainer";
